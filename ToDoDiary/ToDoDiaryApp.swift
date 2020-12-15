778f24fca44d4f7ea6d6d1e5123e42ac42df5bd0
Tatsuya Ishii <tishii2479@gmail.com>
2020-12-15 18:07:28 +0900

    Initial commit

---
+++ ToDoDiary/ToDoDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDiaryApp.swift
+//  ToDoDiary
+//
+//  Created by Tatsuya Ishii on 2020/12/15.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
